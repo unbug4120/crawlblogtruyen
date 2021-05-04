22bf5e7ca6da98ae2d4636532d624426979db63c
unknown <iduongdat@gmail.com>
2021-05-04 16:31:24 +0700

    first commit

---
+++ Getall.php
@@ -0,0 +1,45 @@
+<?php
+
+include 'vendor/autoload.php';
+
+use duongdat\phpSimple\HtmlDomParser;
+
+if (isset($_GET["url"])) {
+    $url = $_GET["url"];
+    $content = getContent($url);
+    $html = HtmlDomParser::str_get_html($content);
+    $links = $html->find('.list-wrap p span a');
+    $arr_link = [];
+    foreach ($links as $key => $result) {
+        $link['chapter_num'] = count($links) - $key;
+        $link['chapter_link'] = 'https://blogtruyen.vn' . $result->href;
+        $link['chapter_img'] = getImgChapter('https://blogtruyen.vn' . $result->href);
+        array_push($arr_link, $link);
+    }
+    header('Content-Type: application/json');
+    echo json_encode($arr_link);
+}
+
+function getContent($url)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13");
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
+
+function getImgChapter($url)
+{
+    $content = getContent($url);
+    $html = HtmlDomParser::str_get_html($content);
+    $images = $html->find('article img');
+    $img = [];
+    foreach ($images as $result) {
+        $img[] = $result->src;
+    }
+    return $img;
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'duongdat/comic',
+  ),
+  'versions' => 
+  array (
+    'duongdat/comic' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'unbug4120/php-simple-html' => 
+    array (
+      'pretty_version' => 'v1.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '02e5e6c9fff56fa2ca1a4dba03235feb5a89c50a',
+    ),
+  ),
+);
